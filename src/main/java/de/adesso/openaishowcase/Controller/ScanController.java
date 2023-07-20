e1fc8275009b4558203232868d06532d205f467d
Demkiv, Lyubomir <Lyubomir.Demkiv@adesso.de>
2023-07-20 18:01:47 +0200

    feat_ui: renamed package

--- src/main/java/de/adesso/openaishowcase/Controller/ScanController.java
+++ src/main/java/de/adesso/openaishowcase/Controller/ScanController.java
@@ -1,4 +1,4 @@
-package de.adesso.openaishowcase.controller;
+package de.adesso.openaishowcase.Controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
--- src/test/java/de/adesso/openaishowcase/Controller/ScanControllerTest.java
+++ src/test/java/de/adesso/openaishowcase/Controller/ScanControllerTest.java
@@ -1,18 +1,14 @@
-package de.adesso.openaishowcase.controller;
+package de.adesso.openaishowcase.Controller;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.logging.Logger;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 public class ScanControllerTest {
