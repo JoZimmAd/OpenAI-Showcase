423617bab0f601c538c997cbd91d3dd9d1b663d8
Demkiv, Lyubomir <Lyubomir.Demkiv@adesso.de>
2023-07-20 17:56:14 +0200

    feat_ui: changed button types

--- src/main/java/de/adesso/openaishowcase/controller/ScanController.java
+++ src/main/java/de/adesso/openaishowcase/controller/ScanController.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,7 +30,7 @@ import java.util.logging.Logger;
 
 import static de.adesso.openaishowcase.Utils.MailUtils.getTextFromMessage;
 
-@RestController
+@Controller
 public class ScanController {
 
     public static final List<Category> CATEGORIES = new ArrayList<Category>(EnumSet.allOf(Category.class));
