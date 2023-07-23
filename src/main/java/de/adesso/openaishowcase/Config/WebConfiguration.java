51b9d4e0630e86195ef3035bcace7a446718c3af
Demkiv, Lyubomir <Lyubomir.Demkiv@adesso.de>
2023-07-23 16:49:50 +0200

    feat_ui: changed ui appearance more customer like

--- src/main/java/de/adesso/openaishowcase/Config/WebConfiguration.java
+++ src/main/java/de/adesso/openaishowcase/Config/WebConfiguration.java
@@ -1,9 +1,33 @@
 package de.adesso.openaishowcase.Config;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class WebConfiguration {
+public class WebConfiguration implements ApplicationContextAware, WebMvcConfigurer {
+
+    private ApplicationContext applicationContext;
+
+
+    public void setApplicationContext(final ApplicationContext applicationContext)
+            throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void addResourceHandlers(@NotNull final ResourceHandlerRegistry registry) {
+        WebMvcConfigurer.super.addResourceHandlers(registry);
+        registry.addResourceHandler("/images/**").addResourceLocations("/images/");
+        registry.addResourceHandler("/css/**").addResourceLocations("/css/");
+    }
+
 }
+
+
--- src/main/java/de/adesso/openaishowcase/OpenAiShowcaseApplication.java
+++ src/main/java/de/adesso/openaishowcase/OpenAiShowcaseApplication.java
@@ -1,8 +1,10 @@
 package de.adesso.openaishowcase;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication
 @EnableAsync
